2f63d4e295f53f3ecb6ce1eec3ac0740498e7809
ManuelCecarelli <manuelcecarelli@gmail.com>
2024-04-13 15:30:01 -0300

    Hacemos dinámico el contenido de 'BookItem'

--- BookChampion Project/src/components/bookitem/BookItem.jsx
+++ BookChampion Project/src/components/bookitem/BookItem.jsx
@@ -1,11 +1,22 @@
-const BookItem = () => {
+import { PropTypes } from "prop-types";
+
+const BookItem = ({ title, author, rating, pageCount }) => {
+
     return (
         <>
-            <h2>100 años de soledad</h2>
-            <h3>Gabriel Garcia Marquez</h3>
-            <div>{Array(5).fill("*").length} estrellas</div>
-            <p>410 páginas</p>
+            <h2>{title}</h2>
+            <h3>{author}</h3>
+            <div>{rating?.length} estrellas</div>
+            <p>{pageCount} páginas</p>
         </>
     );
 };
+
+BookItem.propTypes = {
+    title: PropTypes.string,
+    author: PropTypes.string,
+    pageCount: PropTypes.number,
+    rating: PropTypes.array,
+    };
+
 export default BookItem;
