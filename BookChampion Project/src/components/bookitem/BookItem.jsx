811d9a7f9a7b7a4a426f119f84f45b4353f746cf
ManuelCecarelli <manuelcecarelli@gmail.com>
2024-04-13 19:53:35 -0300

    Ejercicio 1 de BookChampion resuelto

--- BookChampion Project/src/components/bookitem/BookItem.jsx
+++ BookChampion Project/src/components/bookitem/BookItem.jsx
@@ -4,7 +4,7 @@ import { Card } from "react-bootstrap";
 const BookItem = ({ title, author, rating, pageCount, imageUrl }) => {
 
     return (
-        <Card style={{width: "22rem"}}>
+        <Card className="mx-3" style={{width: "22rem"}}>
             <Card.Img
                 height={400}
                 variant="top"
