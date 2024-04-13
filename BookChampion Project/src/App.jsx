95c0ddeca3308fcb704bcffcc53f4dedafd8288c
ManuelCecarelli <manuelcecarelli@gmail.com>
2024-04-13 16:02:33 -0300

    Agregamos e implementamos el componente 'Card' de react-bootstrap en nuestro componente 'BookItem'

--- BookChampion Project/src/App.jsx
+++ BookChampion Project/src/App.jsx
@@ -45,24 +45,28 @@ const App = () => {
         author={books[0].bookAuthor}
         rating={books[0].bookRating}
         pageCount={books[0].pageCount}
+        imageUrl={books[0].imageUrl}
       />
       <BookItem
         title={books[1].bookTitle}
         author={books[1].bookAuthor}
         rating={books[1].bookRating}
         pageCount={books[1].pageCount}
+        imageUrl={books[1].imageUrl}
       />
       <BookItem
         title={books[2].bookTitle}
         author={books[2].bookAuthor}
         rating={books[2].bookRating}
         pageCount={books[2].pageCount}
+        imageUrl={books[2].imageUrl}
       />
       <BookItem
         title={books[3].bookTitle}
         author={books[3].bookAuthor}
         rating={books[3].bookRating}
         pageCount={books[3].pageCount}
+        imageUrl={books[3].imageUrl}
       />
     </>
   )
--- BookChampion Project/src/components/bookitem/BookItem.jsx
+++ BookChampion Project/src/components/bookitem/BookItem.jsx
@@ -1,14 +1,22 @@
 import { PropTypes } from "prop-types";
+import { Card } from "react-bootstrap";
 
-const BookItem = ({ title, author, rating, pageCount }) => {
+const BookItem = ({ title, author, rating, pageCount, imageUrl }) => {
 
     return (
-        <>
-            <h2>{title}</h2>
-            <h3>{author}</h3>
-            <div>{rating?.length} estrellas</div>
-            <p>{pageCount} páginas</p>
-        </>
+        <Card style={{width: "22rem"}}>
+            <Card.Img
+                height={400}
+                variant="top"
+                src={imageUrl !== "" ? imageUrl : "https://bit.ly/47NylZk"}
+            />
+            <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Subtitle>{author}</Card.Subtitle>
+                <div>{rating?.length} estrellas</div>
+                <p>{pageCount} páginas</p>
+            </Card.Body>
+        </Card>
     );
 };
 
@@ -17,6 +25,7 @@ BookItem.propTypes = {
     author: PropTypes.string,
     pageCount: PropTypes.number,
     rating: PropTypes.array,
+    imageUrl: PropTypes.string
     };
 
 export default BookItem;
