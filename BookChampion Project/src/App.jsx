ef17e9afcf1f1ea473eafaf39f405f033a53900e
ManuelCecarelli <manuelcecarelli@gmail.com>
2024-04-13 14:07:02 -0300

    Creamos el nuevo componente 'BookItem.jsx'

--- BookChampion Project/src/App.jsx
+++ BookChampion Project/src/App.jsx
@@ -1,9 +1,12 @@
+import BookItem from "./components/bookitem/BookItem";
+
 const App = () => {
 
   return (
     <>
       <h2>Books Champion App</h2>
       <p>¡Quiero leer libros!</p>
+      <BookItem/>
     </>
   )
 }
