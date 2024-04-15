58a0c162a76bf35a29f2cffc1b9a69066757a73a
ManuelCecarelli <134541962+ManuelCecarelli@users.noreply.github.com>
2024-04-15 15:12:08 -0300

    Modificaciones en clase del 15-04-2024

--- BookChampion Project/src/components/bookitem/BookItem.jsx
+++ BookChampion Project/src/components/bookitem/BookItem.jsx
@@ -1,7 +1,14 @@
 import { PropTypes } from "prop-types";
-import { Card } from "react-bootstrap";
+import { Card, Button } from "react-bootstrap";
+import { useState } from "react";
 
 const BookItem = ({ title, author, rating, pageCount, imageUrl }) => {
+    const [bookTitle,setBookTitle] = useState(title);
+
+    let newTitle = title;
+    const clickHandler = () => {
+        setBookTitle("Actualizado");
+    };
 
     return (
         <Card className="mx-3" style={{width: "22rem"}}>
@@ -11,10 +18,11 @@ const BookItem = ({ title, author, rating, pageCount, imageUrl }) => {
                 src={imageUrl !== "" ? imageUrl : "https://bit.ly/47NylZk"}
             />
             <Card.Body>
-                <Card.Title>{title}</Card.Title>
+                <Card.Title>{bookTitle}</Card.Title>
                 <Card.Subtitle>{author}</Card.Subtitle>
                 <div>{rating?.length} estrellas</div>
                 <p>{pageCount} páginas</p>
+                <Button onClick={clickHandler}>Agregar titulo</Button>
             </Card.Body>
         </Card>
     );
